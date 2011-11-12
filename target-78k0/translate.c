c66b6e32fa9151c253312518a8cd3eea85629618
Andreas Färber <andreas.faerber@web.de>
2011-11-12 11:30:03 +0100

    target-rl78: Flesh out disas
    Signed-off-by: Andreas Färber <andreas.faerber@web.de>

--- target-78k0/translate.c
+++ target-78k0/translate.c
@@ -27,12 +27,52 @@
 #include "tcg-op.h"
 #include "qemu-log.h"
 
+/* global register indexes */
+static TCGv_ptr cpu_env;
+
 #include "helper.h"
 #define GEN_HELPER 1
 #include "helper.h"
 
-//#include "gen-icount.h"
+#include "gen-icount.h"
 
+//#define RL78_DEBUG_DISAS
+
+#ifdef RL78_DEBUG_DISAS
+#  define LOG_DISAS(...) printf(__VA_ARGS__)
+#else
+#  define LOG_DISAS(...) do { } while (0)
+#endif
+
+#define LOG_ASM(...) \
+    if (qemu_loglevel_mask(CPU_LOG_TB_IN_ASM)) { \
+        qemu_log(__VA_ARGS__); \
+    }
+
+typedef struct DisasContext DisasContext;
+struct DisasContext {
+    uint32_t pc;
+    int is_jmp;
+    struct TranslationBlock *tb;
+};
+
+
+static void disas_rl78_insn(DisasContext *s)
+{
+    uint8_t opc;
+    int ins_len = 1;
+
+    opc = ldub_code(s->pc);
+
+    switch (opc) {
+    default:
+        qemu_log("unimplemented opcode 0x%" PRIx8 "\n", opc);
+        // TODO
+        break;
+    }
+
+    s->pc += ins_len;
+}
 
 /* generate intermediate code in gen_opc_buf and gen_opparam_buf for
    basic block 'tb'. If search_pc is TRUE, also generate PC
@@ -41,6 +81,77 @@ static inline void gen_intermediate_code_internal(CPUState *env,
                                                   TranslationBlock *tb,
                                                   bool search_pc)
 {
+    DisasContext dc;
+    target_ulong pc_start;
+    uint16_t *gen_opc_end;
+    int num_insns, max_insns;
+    CPUBreakpoint *bp;
+    int j, lj = -1;
+
+    pc_start = tb->pc;
+    pc_start &= 0x0fffff;
+    dc.pc = pc_start;
+    dc.is_jmp = DISAS_NEXT;
+    dc.tb = tb;
+
+    gen_opc_end = gen_opc_buf + OPC_MAX_SIZE;
+
+    num_insns = 0;
+    max_insns = tb->cflags & CF_COUNT_MASK;
+    if (max_insns == 0) {
+        max_insns = CF_COUNT_MASK;
+    }
+
+    gen_icount_start();
+
+    do {
+        if (unlikely(!QTAILQ_EMPTY(&env->breakpoints))) {
+            QTAILQ_FOREACH(bp, &env->breakpoints, entry) {
+                if (bp->pc == dc.pc) {
+                    // TODO
+                    break;
+                }
+            }
+        }
+        if (search_pc) {
+            j = gen_opc_ptr - gen_opc_buf;
+            if (lj < j) {
+                lj++;
+                while (lj < j) {
+                    gen_opc_instr_start[lj++] = 0;
+                }
+            }
+            gen_opc_pc[lj] = dc.pc;
+            gen_opc_instr_start[lj] = 1;
+            gen_opc_icount[lj] = num_insns;
+        }
+
+        disas_rl78_insn(&dc);
+
+        num_insns++;
+        if (env->singlestep_enabled) {
+            // TODO
+        }
+    } while (dc.is_jmp == DISAS_NEXT
+             && gen_opc_ptr < gen_opc_end && num_insns < max_insns
+             && !env->singlestep_enabled && !singlestep);
+
+    /* Generate the return instruction */
+    if (dc.is_jmp != DISAS_TB_JUMP) {
+        tcg_gen_exit_tb(0);
+    }
+    gen_icount_end(tb, num_insns);
+    *gen_opc_ptr = INDEX_op_end;
+    if (search_pc) {
+        j = gen_opc_ptr - gen_opc_buf;
+        lj++;
+        while (lj <= j) {
+            gen_opc_instr_start[lj++] = 0;
+        }
+    } else {
+        tb->size = dc.pc - pc_start;
+        tb->icount = num_insns;
+    }
 }
 
 void gen_intermediate_code(CPUState *env, TranslationBlock *tb)
